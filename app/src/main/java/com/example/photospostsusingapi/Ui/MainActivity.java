1675c3c63ffc3bfc88236b2bdaf8680ae2dcd9dd
makkawy16 <yousef.elmakkawy@gmail.com>
2022-10-22 09:40:33 +0200

    show response in logcat

--- app/src/main/java/com/example/photospostsusingapi/Ui/MainActivity.java
+++ app/src/main/java/com/example/photospostsusingapi/Ui/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.photospostsusingapi.Ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.photospostsusingapi.R;
+import com.example.photospostsusingapi.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(binding.getRoot());
     }
 }
--- app/src/main/java/com/example/photospostsusingapi/Ui/PostsFragment.java
+++ app/src/main/java/com/example/photospostsusingapi/Ui/PostsFragment.java
@@ -1,19 +1,33 @@
 package com.example.photospostsusingapi.Ui;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.photospostsusingapi.R;
+import com.example.photospostsusingapi.data.Model.PhotoResponseItem;
+import com.example.photospostsusingapi.data.Source.Remote.RetrofitClient;
+import com.example.photospostsusingapi.databinding.FragmentPostsBinding;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class PostsFragment extends Fragment {
+
+    FragmentPostsBinding binding;
+    ProgressDialog mloadingBar ;
 
 
     public PostsFragment() {
@@ -37,5 +51,45 @@ public class PostsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding = FragmentPostsBinding.bind(view);
+
+        fetchPosts();
+    }
+
+    private void fetchPosts() {
+
+        waitnig("Loading");
+        RetrofitClient.getWebService().getPhotos().enqueue(new Callback<List<PhotoResponseItem>>() {
+            @Override
+            public void onResponse(Call<List<PhotoResponseItem>> call, Response<List<PhotoResponseItem>> response) {
+                Log.d("dddddd", "onResponse: " + response.body());
+                mloadingBar.dismiss();
+            }
+
+            @Override
+            public void onFailure(Call<List<PhotoResponseItem>> call, Throwable t) {
+                Log.d("dddddddddd", "onFailure: " + t.getLocalizedMessage());
+                mloadingBar.dismiss();
+            }
+        });
+
+    }
+
+
+    private void waitnig(String title) {
+        mloadingBar = new ProgressDialog(getContext());
+        mloadingBar.setTitle(title);
+        mloadingBar.setMessage("Please Wait");
+        mloadingBar.setCanceledOnTouchOutside(false);
+        mloadingBar.show();
+    }
+
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding=null;
     }
 }
