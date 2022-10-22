b910b6a4b5a4428b171042cfa5f6713e5a926084
makkawy16 <yousef.elmakkawy@gmail.com>
2022-10-22 09:01:59 +0200

    inital commit

---
+++ app/src/main/java/com/example/photospostsusingapi/Ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.photospostsusingapi.Ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.photospostsusingapi.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
